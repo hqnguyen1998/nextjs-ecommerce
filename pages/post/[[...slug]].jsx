d56ce8f0609df716b9dd45230b021ae878ef9092
huy nguyen <huynguyen@huys-MacBook-Pro.local>
2020-10-20 23:31:52 -0500

    Edit [[...slug]].jsx

--- pages/post/[[...slug]].jsx
+++ pages/post/[[...slug]].jsx
@@ -9,33 +9,29 @@ const Post = ({ post }) => {
   );
 };
 
-// export const getStaticPaths = async () => {
-//   const { data } = await axios.get(`${process.env.API_URL}/api/post`);
+export const getStaticPaths = async () => {
+  const { data } = await axios.get(`${process.env.API_URL}/api/post`);
 
-//   const paths = data.data.map((post) => ({
-//     params: { slug: [post.author, post.slug] },
-//   }));
+  const paths = data.data.map((post) => ({
+    params: { slug: [post.author, post.slug] },
+  }));
 
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// };
+  return {
+    paths,
+    fallback: false,
+  };
+};
 
-// export const getStaticProps = async ({ params }) => {
-//   const { data } = await axios({
-//     url: `${process.env.API_URL}/api/post/${params.slug[1]}`,
-//     method: 'GET',
-//     headers: {
-//       'Content-type': 'application/json',
-//     },
-//   });
+export const getStaticProps = async ({ params }) => {
+  const { data } = await axios.get(
+    `${process.env.API_URL}/api/post/${params.slug[1]}`
+  );
 
-//   return {
-//     props: {
-//       post: data.data,
-//     },
-//   };
-// };
+  return {
+    props: {
+      post: data.data,
+    },
+  };
+};
 
 export default Post;
