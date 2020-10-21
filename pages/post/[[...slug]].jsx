bc6547a42568dc4ca97b4eb935801afbd1ea5df5
huy nguyen <huynguyen@huys-MacBook-Pro.local>
2020-10-20 23:14:51 -0500

    Undo post/slug index.jsx

--- pages/post/[[...slug]].jsx
+++ pages/post/[[...slug]].jsx
@@ -1,41 +1,41 @@
 import axios from 'axios';
 import MainLayout from '../../layouts/mainLayout';
 
-const Post = () => {
+const Post = ({ post }) => {
   return (
-    <MainLayout title='Post'>
-      <h1>Post</h1>
+    <MainLayout title={post.title}>
+      <h1>{post.title}</h1>
     </MainLayout>
   );
 };
 
-// export const getStaticPaths = async () => {
-//   const { data } = await axios.get(`${process.env.API_URL}/api/post`);
+export const getStaticPaths = async () => {
+  const { data } = await axios.get(`${process.env.API_URL}/api/post`);
 
-//   const paths = data.data.map((post) => ({
-//     params: { slug: [post.author, post.slug] },
-//   }));
+  const paths = data.data.map((post) => ({
+    params: { slug: [post.author, post.slug] },
+  }));
 
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// };
+  return {
+    paths,
+    fallback: false,
+  };
+};
 
-// export const getStaticProps = async ({ params }) => {
-//   const { data } = await axios({
-//     url: `${process.env.API_URL}/api/post/${params.slug[1]}`,
-//     method: 'GET',
-//     headers: {
-//       'Content-type': 'application/json',
-//     },
-//   });
+export const getStaticProps = async ({ params }) => {
+  const { data } = await axios({
+    url: `${process.env.API_URL}/api/post/${params.slug[1]}`,
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json',
+    },
+  });
 
-//   return {
-//     props: {
-//       post: data.data,
-//     },
-//   };
-// };
+  return {
+    props: {
+      post: data.data,
+    },
+  };
+};
 
 export default Post;
