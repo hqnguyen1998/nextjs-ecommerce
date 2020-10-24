871a65d7f6fa4d15ff5f0b903b90668ab8868af2
huy nguyen <huynguyen@huys-MacBook-Pro.local>
2020-10-23 22:24:20 -0500

    Remove NODE_ENV in next.config.js

--- next.config.js
+++ next.config.js
@@ -8,6 +8,5 @@ module.exports = withSass({
     API_URL: process.env.API_URL,
     JWT_SECRET: process.env.JWT_SECRET,
     MONGO_URI: process.env.MONGO_URI,
-    NODE_ENV: process.env.NODE_ENV,
   },
 });
